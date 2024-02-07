3a1a34ee75a8f6fbc6810fd063ce948275478315
Rory Doak <rory.doak@gmail.com>
2024-02-07 10:53:55 +0000

    FE1.1 add hero and backgrnd image

--- src/components/HomeHero/HomeHero.tsx
+++ src/components/HomeHero/HomeHero.tsx
@@ -5,16 +5,11 @@ import NavButton from "../NavButton/NavButton";
 export default function HomeHero() {
   return (
     <section className="hero">
-      <img
-        src={birdsEyeBattle}
-        alt="birds eye view of a warhammer battle table"
-        className="hero__img"
-      />
       <h1 className="hero__title">
         The Crypt is a Warhammer gaming club in the East Neuk of Fife
       </h1>
       <div className="hero__button-wrap">
-        <NavButton colour="blue" text="Member's Log In" page="" />
+        <NavButton colour="blue" text="Log In" page="" />
         <NavButton colour="dark" text="Sign Up" page="" />
       </div>
     </section>
