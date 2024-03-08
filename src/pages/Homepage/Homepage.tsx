9da39c9f64edb90c83fb484f3d59b6794489020d
Rory Doak <rory.doak@gmail.com>
2024-03-08 13:28:59 +0000

    continuing request testing

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,6 +1,6 @@
 // import CompletedBattles from "../../components/CompletedBattles/CompletedBattles";
 // import FantasyTopFive from "../../components/FantasyTopFive/FantasyTopFive ";
-// import FortyKTopFive from "../../components/FortyKTopFive/FortyKTopFive";
+import FortyKTopFive from "../../components/FortyKTopFive/FortyKTopFive";
 import Header from "../../components/Header/Header";
 import HomeHero from "../../components/HomeHero/HomeHero";
 import NavFooter from "../../components/NavFooter/NavFooter";
@@ -13,8 +13,8 @@ export default function Homepage() {
       <Header />
       <HomeHero />
       <UpcomingBattles />
-      {/* <FortyKTopFive />
-      <FantasyTopFive />
+      <FortyKTopFive />
+      {/* <FantasyTopFive />
       <CompletedBattles /> */}
       <NavFooter />
     </main>
