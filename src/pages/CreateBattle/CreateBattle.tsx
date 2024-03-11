db4037ddb883eb4cb01cc2451933e7c111e662e0
Rory Doak <rory.doak@gmail.com>
2024-03-11 15:56:31 +0000

    add error loop for getallarmies fn

--- src/pages/CreateBattle/CreateBattle.tsx
+++ src/pages/CreateBattle/CreateBattle.tsx
@@ -64,6 +64,11 @@ export default function CreateBattle() {
       if (!armyArray) {
         const response = await getAllArmies();
         console.log({ armyResponse: response });
+        if (!response) {
+          const response = await getAllArmies();
+          setArmyArray(response);
+          return response;
+        }
         setArmyArray(response);
         console.log("army request");
         const filteredResponse = response.filter(
