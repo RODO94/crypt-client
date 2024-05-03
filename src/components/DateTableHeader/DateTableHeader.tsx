4c3ebde5d62c6dfd4f25e22461a953e88fa8c2a8
Rory Doak <rory.doak@gmail.com>
2024-05-03 17:20:54 +0100

    add year to date header

--- src/components/DateTableHeader/DateTableHeader.tsx
+++ src/components/DateTableHeader/DateTableHeader.tsx
@@ -8,7 +8,7 @@ interface DateTableHeader {
 export default function DateTableHeader({ date }: DateTableHeader) {
   return (
     <div className="dateheader">
-      {dayjs(date, "YYYY-MM-DD").format("dddd DD/MM")}
+      {dayjs(date, "YYYY-MM-DD").format("dddd DD/MM/YY")}
     </div>
   );
 }
