3298ee56d88801248301aaaf9e55800afcb119e4
Rory Doak <rory.doak@gmail.com>
2025-03-23 14:56:25 +0000

    refine fetching of army info data and remove component state

--- src/pages/ArmyInfo/ArmyInfo.tsx
+++ src/pages/ArmyInfo/ArmyInfo.tsx
@@ -1,21 +1,14 @@
 import { useLocation } from "react-router-dom";
 import "./ArmyInfo.scss";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import ArmyDash from "../../components/ArmyDash/ArmyDash";
 import ArmyNemesis from "../../components/ArmyNemesis/ArmyNemesis";
 import ArmyAlly from "../../components/ArmyAlly/ArmyAlly";
-import { Player } from "../../utils/Interfaces";
 import { CircularProgress } from "@mui/material";
 import RankGraph from "../../components/RankGraph/RankGraph";
 import { useArmiesStore } from "../../store/armies";
 
 export default function ArmyInfo() {
-  const [battleCount, setBattleCount] = useState(0);
-  const [winPercent, setWinPercent] = useState("");
-  const [armyRank, setArmyRank] = useState(0);
-  const [nemesisObj, setNemesisObj] = useState<Player | null>(null);
-  const [allyObj, setAllyObj] = useState<Player | null>(null);
-
   const location = useLocation();
   const { fetchArmyDetails, selectedArmy } = useArmiesStore();
 
@@ -23,15 +16,7 @@ export default function ArmyInfo() {
 
   useEffect(() => {
     const fetchData = async () => {
-      if (!selectedArmy) {
-        await fetchArmyDetails(armyID);
-      } else {
-        setBattleCount(selectedArmy.battleCount);
-        setWinPercent(selectedArmy.winPercent);
-        setArmyRank(Number(selectedArmy.user.ranking));
-        setNemesisObj(selectedArmy.nemesis);
-        setAllyObj(selectedArmy.ally);
-      }
+      await fetchArmyDetails(armyID);
     };
 
     fetchData();
@@ -48,16 +33,16 @@ export default function ArmyInfo() {
   return (
     <main className="army-info">
       <ArmyDash
-        winPercent={winPercent}
-        battleCount={battleCount}
+        winPercent={selectedArmy.winPercent}
+        battleCount={selectedArmy.battleCount}
         armyObj={selectedArmy.user}
-        armyRank={armyRank}
+        armyRank={Number(selectedArmy.user.ranking)}
       />
       <RankGraph army_id={selectedArmy.user.id} name={selectedArmy.user.name} />
       <section className="army-info__nemesis-ally">
         <div className="army-info__container">
-          <ArmyNemesis nemesis={nemesisObj} />
-          <ArmyAlly ally={allyObj} />
+          <ArmyNemesis nemesis={selectedArmy.nemesis} />
+          <ArmyAlly ally={selectedArmy.ally} />
         </div>
       </section>
     </main>
