a61f94278b33f724415bd3fd10c473693bb427c1
Rory Doak <rory.doak@gmail.com>
2025-03-25 13:06:10 +0000

    add initial styling and content

--- src/components/RankTracker/RankTracker.tsx
+++ src/components/RankTracker/RankTracker.tsx
@@ -4,10 +4,27 @@ import "./RankTracker.scss";
 
 export default function RankTracker({ rankings }: { rankings: Rank[] }) {
   return (
-    <section>
-      {rankings.map((ranking: Rank) => (
-        <RankTrackerCard ranking={ranking} />
-      ))}
+    <section className="rank-tracker">
+      {rankings.sort(sortRankings()).map((ranking: Rank, index: number) => {
+        const previousRanking = rankings[index + 1] || null;
+
+        return (
+          <RankTrackerCard
+            key={ranking.id}
+            ranking={ranking}
+            previousRanking={previousRanking}
+          />
+        );
+      })}
     </section>
   );
+}
+function sortRankings(): ((a: Rank, b: Rank) => number) | undefined {
+  return (a: Rank, b: Rank) => {
+    if (a.rn && b.rn) {
+      return a.rn - b.rn;
+    } else {
+      return 0;
+    }
+  };
 }
--- src/components/RankTrackerCard/RankTrackerCard.tsx
+++ src/components/RankTrackerCard/RankTrackerCard.tsx
@@ -1,10 +1,70 @@
+import dayjs from "dayjs";
 import { Rank } from "../../utils/Interfaces";
 import "./RankTrackerCard.scss";
+import Chip, { ChipOwnProps } from "@mui/material/Chip";
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
+import TrendingDownIcon from "@mui/icons-material/TrendingDown";
+import TrendingFlatIcon from "@mui/icons-material/TrendingFlat";
 
-export default function RankTrackerCard({ ranking }: { ranking: Rank }) {
+interface RankCardProps {
+  ranking: Rank;
+  previousRanking: Rank | null;
+}
+
+interface RankMessage {
+  message: "Increase" | "Decrease" | "Unchanged";
+  color: ChipOwnProps["color"];
+  icon: JSX.Element | null;
+}
+
+export default function RankTrackerCard({
+  ranking,
+  previousRanking,
+}: RankCardProps) {
+  const handleRankChangeMessage = (): RankMessage => {
+    const rankNumber = Number(ranking.ranking);
+    const prevRankNumber = Number(previousRanking?.ranking);
+
+    if (rankNumber > prevRankNumber)
+      return {
+        message: "Increase",
+        color: "success",
+        icon: <TrendingUpIcon color={"success"} />,
+      };
+    if (rankNumber < prevRankNumber)
+      return {
+        message: "Decrease",
+        color: "error",
+        icon: <TrendingDownIcon color="error" />,
+      };
+
+    return {
+      message: "Unchanged",
+      color: "info",
+      icon: <TrendingFlatIcon color="info" />,
+    };
+  };
+
   return (
-    <article className="rank-tracker__card">
-      <p>{ranking.ranking}</p>
+    <article className="rank-tracker-card">
+      <div className="rank-tracker-card__main">
+        <div className="rank-tracker-card__main-text">
+          <p className="rank-tracker-card__ranking">{ranking.ranking}</p>
+          <p className="rank-tracker-card__old-ranking">
+            {previousRanking?.ranking || "-"}
+          </p>
+        </div>
+        {handleRankChangeMessage().icon}
+      </div>
+      <div className="rank-tracker-card__banner">
+        <p>{dayjs(ranking.date).format("DD/MM")}</p>
+        <Chip
+          label={handleRankChangeMessage().message}
+          variant="filled"
+          color={handleRankChangeMessage().color}
+          sx={{ font: "inherit", height: "auto", paddingY: "0.25rem" }}
+        />
+      </div>
     </article>
   );
 }
