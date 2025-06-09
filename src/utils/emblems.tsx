5c8218b4337ffc4f85fa21d3f213e6ef9d071769
Rory Doak <rory.doak@gmail.com>
2025-06-09 14:16:53 +0100

    refactor emblem components to use object rather than nested ternary

---
+++ src/utils/emblems.tsx
@@ -0,0 +1,123 @@
+import adeptasororitas from "../assets/emblems/adepta-sororitas-crest.svg";
+import adeptuscustodes from "../assets/emblems/adeptus-custodes-crest.svg";
+import adeptusmechanicus from "../assets/emblems/adeptus-mechanicus-crest.svg";
+import aelderi from "../assets/emblems/aelderi-crest.svg";
+import astramilitarum from "../assets/emblems/astra-militarum-crest.svg";
+import badmoons from "../assets/emblems/bad-moons.svg";
+import barrowkings from "../assets/emblems/barrowkings.svg";
+import beastmen from "../assets/emblems/beastmen.svg";
+import blackdragons from "../assets/emblems/black-dragons.svg";
+import bloodangels from "../assets/emblems/bloodangels-crest.svg";
+import centurionhonor from "../assets/emblems/centurion-honour.svg";
+import chaosspacemarines from "../assets/emblems/chaos-space-marines-crest.svg";
+import chaosdemons from "../assets/emblems/chaosdemons-crest.svg";
+import daemonsofkhorne from "../assets/emblems/daemons-of-khorne.svg";
+import daemonsofnurgle from "../assets/emblems/daemons-of-nurgle.svg";
+import darkelves from "../assets/emblems/dark-elves.svg";
+import darkangels from "../assets/emblems/darkangels-crest.svg";
+import darktemplars from "../assets/emblems/darktemplars-crest.svg";
+import deathguard from "../assets/emblems/deathguard-crest.svg";
+import deathwatch from "../assets/emblems/deathwatch-crest.svg";
+import drukhari from "../assets/emblems/drukhari-crest.svg";
+import dwarf from "../assets/emblems/dwarf.svg";
+import empire from "../assets/emblems/empire.svg";
+import falshia from "../assets/emblems/falshia.svg";
+import genestealercult from "../assets/emblems/genestealer-cult-crest.svg";
+import greyknights from "../assets/emblems/greyknights-crest.svg";
+import highelves from "../assets/emblems/high-elves.svg";
+import leaguesofvotann from "../assets/emblems/leaguesofvotann-crest.svg";
+import lizardmen from "../assets/emblems/lizardmen.svg";
+import kroot from "../assets/emblems/kroot.svg";
+import metalica from "../assets/emblems/metalica.svg";
+import necrons from "../assets/emblems/necrons-crest.svg";
+import nightgoblins from "../assets/emblems/night-goblins.svg";
+import ogrekingdoms from "../assets/emblems/ogre-kingdoms.svg";
+import orcsandgoblins from "../assets/emblems/orcs-and-goblins.svg";
+import orks from "../assets/emblems/orks-crest.svg";
+import savageorcs from "../assets/emblems/savage-orcs.svg";
+import skaven from "../assets/emblems/skaven.svg";
+import slaanesh from "../assets/emblems/slaanesh.svg";
+import spacemarines from "../assets/emblems/spacemarines-crest.svg";
+import spacewolves from "../assets/emblems/spacewolves-crest.svg";
+import tau from "../assets/emblems/t'au-crest.svg";
+import thousandsons from "../assets/emblems/thousandsons-crest.svg";
+import tyrannids from "../assets/emblems/tyrannids-crest.svg";
+import tzeentchdaemons from "../assets/emblems/tzeentch-daemons.svg";
+import vampirecounts from "../assets/emblems/vampire-counts.svg";
+import warpspiders from "../assets/emblems/warp-spiders.svg";
+import warriorsofchaoskhorne from "../assets/emblems/warriors-of-chaos-khorne.svg";
+import warriorsofchaosnurgle from "../assets/emblems/warriors-of-chaos-nurgle.svg";
+import warriorsofchaostzeentch from "../assets/emblems/warriors-of-chaos-tzeentch.svg";
+import warriorsofchaos from "../assets/emblems/warriors-of-chaos.svg";
+import woodelves from "../assets/emblems/wood-elves.svg";
+import worldeaters from "../assets/emblems/worldeaters-crest.svg";
+import ultramarines from "../assets/emblems/ultramarines.svg";
+import salamanders from "../assets/emblems/salamanders.svg";
+import whitescars from "../assets/emblems/whitescars.svg";
+import snakebites from "../assets/emblems/snakebites.svg";
+import gofforks from "../assets/emblems/gofforks.svg";
+import bloodaxes from "../assets/emblems/bloodaxes.svg";
+
+export const emblems = {
+  adeptasororitas: adeptasororitas,
+  adeptuscustodes: adeptuscustodes,
+  adeptusmechanicus: adeptusmechanicus,
+  aelderi: aelderi,
+  astramilitarum: astramilitarum,
+  badmoons: badmoons,
+  barrowkings: barrowkings,
+  beastmen: beastmen,
+  blackdragons: blackdragons,
+  bloodangels: bloodangels,
+  centurionhonor: centurionhonor,
+  chaosspacemarines: chaosspacemarines,
+  chaosdemons: chaosdemons,
+  daemonsofkhorne: daemonsofkhorne,
+  daemonsofnurgle: daemonsofnurgle,
+  darkelves: darkelves,
+  darkangels: darkangels,
+  darktemplars: darktemplars,
+  deathguard: deathguard,
+  deathwatch: deathwatch,
+  drukhari: drukhari,
+  dwarf: dwarf,
+  empire: empire,
+  falshia: falshia,
+  genestealercult: genestealercult,
+  greyknights: greyknights,
+  highelves: highelves,
+  leaguesofvotann: leaguesofvotann,
+  lizardmen: lizardmen,
+  kroot: kroot,
+  metalica: metalica,
+  necrons: necrons,
+  nightgoblins: nightgoblins,
+  ogrekingdoms: ogrekingdoms,
+  orcsandgoblins: orcsandgoblins,
+  orks: orks,
+  savageorcs: savageorcs,
+  skaven: skaven,
+  slaanesh: slaanesh,
+  spacemarines: spacemarines,
+  spacewolves: spacewolves,
+  tau: tau,
+  thousandsons: thousandsons,
+  tyrannids: tyrannids,
+  tzeentchdaemons: tzeentchdaemons,
+  vampirecounts: vampirecounts,
+  warpspiders: warpspiders,
+  warriorsofchaoskhorne: warriorsofchaoskhorne,
+  warriorsofchaosnurgle: warriorsofchaosnurgle,
+  warriorsofchaostzeentch: warriorsofchaostzeentch,
+  warriorsofchaos: warriorsofchaos,
+  worldeaters: worldeaters,
+  ultramarines: ultramarines,
+  salamanders: salamanders,
+  whitescars: whitescars,
+  snakebites: snakebites,
+  gofforks: gofforks,
+  bloodaxes: bloodaxes,
+  woodelves: woodelves,
+};
+
+export type Emblems = keyof typeof emblems;
