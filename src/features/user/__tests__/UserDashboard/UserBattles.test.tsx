26481fa9843c4aa93fb6fc9bfe22476d7926d5f8
Rory Doak <rory.doak@gmail.com>
2025-06-10 11:32:45 +0100

    initial test scaffolding and todos for User Dash Hero and User Rankings

---
+++ src/features/user/__tests__/UserDashboard/UserBattles.test.tsx
@@ -0,0 +1,11 @@
+describe("the Users Upcoming Battle", () => {
+  it.todo("Renders a list of upcoming battles");
+  it.todo("only shows fantasy armies the user owns");
+  it.todo("the fantasy rankings show top rank first");
+});
+
+describe("the Users 40k Rankings", () => {
+  it.todo("Renders two tables for the users rankings");
+  it.todo("only shows 40k armies the user owns");
+  it.todo("the fantasy 40k show top rank first");
+});
---
+++ src/features/user/__tests__/UserDashboard/UserRankings.test.tsx
@@ -0,0 +1,15 @@
+describe("the main UI features", () => {
+  it.todo("Renders two tables for the users rankings");
+});
+
+describe("the Users Fantasy Rankings", () => {
+  it.todo("Renders two tables for the users rankings");
+  it.todo("only shows fantasy armies the user owns");
+  it.todo("the fantasy rankings show top rank first");
+});
+
+describe("the Users 40k Rankings", () => {
+  it.todo("Renders two tables for the users rankings");
+  it.todo("only shows 40k armies the user owns");
+  it.todo("the fantasy 40k show top rank first");
+});
