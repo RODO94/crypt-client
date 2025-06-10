a16e2870ed3f3b707c3f410d82da22653bf547b8
Rory Doak <rory.doak@gmail.com>
2025-06-10 11:48:32 +0100

    finish scaffolding with it.dos in UserDashboard tests

--- src/features/user/__tests__/UserDashboard/UserBattles.test.tsx
+++ src/features/user/__tests__/UserDashboard/UserBattles.test.tsx
@@ -1,11 +1,9 @@
 describe("the Users Upcoming Battle", () => {
   it.todo("Renders a list of upcoming battles");
-  it.todo("only shows fantasy armies the user owns");
-  it.todo("the fantasy rankings show top rank first");
+  it.todo("the next upcoming battle is shown at the top");
 });
 
-describe("the Users 40k Rankings", () => {
-  it.todo("Renders two tables for the users rankings");
-  it.todo("only shows 40k armies the user owns");
-  it.todo("the fantasy 40k show top rank first");
+describe("the Users Recent Results", () => {
+  it.todo("Renders a list of recent results");
+  it.todo("it shows the latest one first");
 });
