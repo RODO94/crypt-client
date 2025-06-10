4a9355d6872a0cf01ce87800b49ef36ab8aea3a5
Rory Doak <rory.doak@gmail.com>
2025-06-10 13:53:23 +0100

    update spec and config

--- e2e/example/test.spec.tsx
+++ e2e/example/test.spec.tsx
@@ -4,7 +4,8 @@ test.describe("an example test", () => {
   test("a user ", async ({ page }) => {
     await page.goto("/");
     await page.waitForSelector("h1");
-    const header = await page.getByText(
+    console.log(page.mainFrame());
+    const header = page.getByText(
       "The Crypt is a Warhammer gaming club in the East Neuk of Fife"
     );
     expect(header).toBeVisible();
